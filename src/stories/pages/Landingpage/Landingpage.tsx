0754899a19a1da9748ad58d4a1fe2a03d83f9be0
alicepetzinger <alice.petzinger@gmail.com>
2021-11-25 17:27:31 +0100

    🎉 add background picture and headline

--- src/stories/pages/Landingpage/Landingpage.tsx
+++ src/stories/pages/Landingpage/Landingpage.tsx
@@ -3,12 +3,11 @@ import styles from "./Landingpage.module.css";
 
 export default function Landingpage() {
   return (
-    <div>
-      <h1>Visit Rosenheim</h1>
-      <img
-        src="src/assets/landingpage_visitrosenheim.jpeg"
-        alt="landingpage background"
-      />
+    <div className={styles.main}>
+      <div className={styles.headerContainer}>
+        <h1>Visit Rosenheim</h1>
+      </div>
+      <div className={styles.imageContainer}></div>
     </div>
   );
 }
