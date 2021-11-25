8ba4455a7f6ee072f060cd2e24a5dcbcce76d119
alicepetzinger <alice.petzinger@gmail.com>
2021-11-25 15:26:17 +0100

    ➕ add landingpage and basic codes

---
+++ src/stories/pages/Landingpage/Landingpage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Landingpage.module.css";
+
+export default function Landingpage() {
+  return (
+    <div>
+      <h1>Visit Rosenheim</h1>
+      <img
+        src="src/assets/landingpage_visitrosenheim.jpeg"
+        alt="landingpage background"
+      />
+    </div>
+  );
+}
---
+++ src/stories/pages/Landingpage/Landingpages.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Landingpage from "./Landingpage";
+
+export default {
+  component: "Landingpage",
+  title: "Pages/landingpage",
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Default = () => <Landingpage />;
