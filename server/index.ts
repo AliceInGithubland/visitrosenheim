0bbb3ca3216c5a1f1033d3be34fa105e67608465
alicepetzinger <alice.petzinger@gmail.com>
2021-12-03 12:11:19 +0100

    😎 Posting Content in my mongodb collection

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import path from "path";
-import { cafeLocation } from "./utils/cafeLocationContent.json";
 import { connectDatabase, getCafeLocationCollection } from "./database";
 
 if (!process.env.MONGODB_URI) {
