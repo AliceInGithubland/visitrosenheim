3d26d88bf6c458bdf56b00758461733d23892745
alicepetzinger <alice.petzinger@gmail.com>
2021-12-08 10:13:04 +0100

    Add CafePage components

---
+++ src/stories/components/X/X.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./X.module.css";
+
+export default function X() {
+  return (
+    <div className={styles.X}>
+      <img src="src/assets/X.svg" />
+    </div>
+  );
+}
---
+++ src/stories/pages/CafePage/CafePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./CafePage.module.css";
+import X from "../../components/X/X";
+
+export default function CafePage() {
+  return (
+    <div className={styles.container}>
+      <X />
+    </div>
+  );
+}
