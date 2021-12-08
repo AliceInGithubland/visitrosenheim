ae45652c62890641fc4031bf5d3087531358fc3b
alicepetzinger <alice.petzinger@gmail.com>
2021-12-08 10:10:21 +0100

    Add DetailcardCafe

---
+++ src/stories/components/DetailcardCafe/DetailcardCafe.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DetailcardCafe from "./DetailcardCafe";
+
+export default {
+  component: DetailcardCafe,
+  title: "Components/DetailcardCafe",
+};
+
+export const Default = () => <DetailcardCafe />;
