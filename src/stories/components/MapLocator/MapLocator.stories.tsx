a6adf6ca95ffe18a5130e84e16e790f2b034c7b9
alicepetzinger <alice.petzinger@gmail.com>
2021-12-15 18:07:14 +0100

    ➕ Add Imports to CafeMap

--- src/stories/components/MapLocator/MapLocator.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MapLocator from "./MapLocator";
-
-export default {
-  component: MapLocator,
-  title: "Components/MapLocator",
-};
-
-export const Default = () => <MapLocator />;
