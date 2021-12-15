d96232d44cf5cb0927f1b7fa1303c684c3e18f6f
alicepetzinger <alice.petzinger@gmail.com>
2021-12-15 14:45:29 +0100

    ➕ Add ArrowBack Component

---
+++ src/stories/components/ArrowBack/ArrowBack.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./ArrowBack.module.css";
+
+export default function ArrowBack() {
+  return (
+    <div className={styles.ArrowBack}>
+      <img src="src/assets/ArrowBack.svg" />
+    </div>
+  );
+}
