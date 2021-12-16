6138d793b2858abfd3e5414c083f76fe1508e862
alicepetzinger <alice.petzinger@gmail.com>
2021-12-16 16:14:02 +0100

    🚚 add routing

--- src/stories/pages/CafeMap/CafeMap.stories.tsx
+++ src/stories/pages/CafeMap/CafeMap.stories.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CafeMap from "./CafeMap";
 
 export default {
--- src/stories/pages/CafeMap/CafeMap.tsx
+++ src/stories/pages/CafeMap/CafeMap.tsx
@@ -1,12 +1,10 @@
-import React from "react";
 import ArrowBack from "../../components/ArrowBack/ArrowBack";
 import X from "../../components/X/X";
-import styles from "src/stories/pages/CafeMap/CafeMap.module.css";
 import Map from "../../components/Map/Map";
 
 export default function CafeMap() {
   return (
-    <div className={styles.container}>
+    <div>
       <Map />
       <ArrowBack />
       <X />
